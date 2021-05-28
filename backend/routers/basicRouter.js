4714869d9c96dcd7a58fd8ca5a7529aa5031fca6
=kncr <=kincirfatih@gmail.com>
2021-05-29 01:07:12 +0200

    add mongo db connection and first api

--- backend/routers/basicRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { getData } from '../controllers/basicControllers.js';
-const basicRouter = express.Router();
-
-basicRouter.route('/').get(getData);
-
-export default basicRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 import express from 'express';
 import { config } from 'dotenv';
-// import connectDB from './config/db.js';
+import connectDB from './config/db.js';
 import colors from 'colors';
+import userRouter from './routers/userRouter.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-import basicRouter from './routers/basicRouter.js';
 
 config();
-// connectDB();
+connectDB();
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 
 /** Routes */
-app.get('/', (req, res) => res.send('App started'));
+// app.get('/', (req, res) => res.json({
+//     autor:"hiroimono",
+//     message:"server running in port 5000 "
+// }));
 
 /** Middlewares */
-app.use('/api', basicRouter);
+app.use('/api/users', userRouter);
+
+/** Not found pages and Error Handler Middlewares */
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Backend Sever is running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow.bold))
