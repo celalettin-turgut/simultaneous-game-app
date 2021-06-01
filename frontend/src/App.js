160df899ccc80658fad17b40da540f254fb9dbbf
Celalettin Turgut <celalettin.turgut@hotmail.com>
2021-06-01 23:12:29 +0200

    This is my first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {console.log('This is the first commit from Celalettin')}
         <a
           className="App-link"
           href="https://reactjs.org"
